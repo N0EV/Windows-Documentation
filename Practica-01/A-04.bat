6eae8d3ef63abf7863c944a9837843ca40976ce2
Noe <noevalladolidescudero@gmail.com>
2024-05-27 23:26:48 +0200

    Actualizacion random

--- Practica-01/A-04.bat
+++ Practica-01/A-04.bat
@@ -1,4 +1,18 @@
 @echo off
+setlocal enabledelayedexpansion
 set num=%1
+set tab=0
+:: Compruebo que me haya pasado algo el usuario
+if "%num%" equ "" (
+	echo Uso: %0 "numero que quiera hacer la tabla de multiplicar"
+	goto final
+)
 echo Ahora vamos a hacer la tabla de multiplicar de %1
-
+for %%a in %1 do (
+	echo Vamos a empezar la multiplicacion
+	set /a mult=%num% * %tab%
+	echo %mult%
+	set /a tabl=!tabl!+1
+)
+:final
+pause
