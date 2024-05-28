39be796028dedb80d31818765a5b1814d2f3e0ea
Noe <noevalladolidescudero@gmail.com>
2024-05-28 15:54:09 +0200

    Actualizado

--- Practica-01/A-01.bat
+++ Practica-01/A-01.bat
@@ -2,10 +2,10 @@
 setlocal enabledelayedexpansion
 rem Para poder hacer la cuenta bien hay que poner lo de arriba "setlocal enabledelayexpansion"
 rem y hacer una variable para contar
-set cont=1
+set cont=0
 rem NOTA: en los for se pone "%%" para la variable y solo admite un caracter
 for %%n in (*.txt) do (
-	echo !cont!-fichero: %%n
 	set /a cont=!cont!+1
+	echo !cont!-fichero: %%n
 )
 echo En este directorio hay !cont! ficheros
--- Practica-01/A-02.bat
+++ Practica-01/A-02.bat
@@ -2,18 +2,18 @@
 setlocal enabledelayedexpansion
 rem Para poder hacer la cuenta bien hay que poner lo de arriba "setlocal enabledelayexpansion"
 rem y hacer una variable para contar
-set cont=1
+set cont=0
 :: Ahora el usuario podra elegir la extension que quiera el por parametro
 set exten=%1
 :: Comprobamos si nos ha pasado algo y si no es asi usaremos .txt por defecto
 if "%1" equ "" (
-	set defect=txt
+	set exten=txt
 ) else (
-	set=%1
+	set exten=%1
 )
 rem NOTA: en los for se pone "%%" para la variable y solo admite un caracter
-for %%n in (*.%defect%) do (
-	echo !cont!-fichero: %%n
+for %%n in (*.%exten%) do (
 	set /a cont=!cont!+1
+	echo !cont!-fichero: %%n
 )
 echo En este directorio hay !cont! ficheros
