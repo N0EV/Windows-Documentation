c83fba4fe002d6615b083600951be488a7855ae4
Noe <noevalladolidescudero@gmail.com>
2024-05-28 16:19:25 +0200

    Actualizado

--- Practica-01/A-04.bat
+++ Practica-01/A-04.bat
@@ -1,18 +1,22 @@
 @echo off
 setlocal enabledelayedexpansion
 set num=%1
-set tab=0
+set mult=0
 :: Compruebo que me haya pasado algo el usuario
 if "%num%" equ "" (
 	echo Uso: %0 "numero que quiera hacer la tabla de multiplicar"
 	goto final
 )
+
 echo Ahora vamos a hacer la tabla de multiplicar de %1
-for %%a in (1,1,10) do (
-	echo Vamos a empezar la multiplicacion
-	set /a mult=%num% * %tab%
-	echo %mult%
-	set /a tabl=!tabl!+1
+echo Vamos a empezar la multiplicacion
+for /l %%a in (1,1,10) do (
+	set /a mult=%num% * %%a
+	echo %num% * %%a es !mult!
 )
+goto fin
+
 :final
 pause
+
+:fin
